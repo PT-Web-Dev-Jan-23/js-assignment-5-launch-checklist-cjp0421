f79abc4e5fa6f61ec700cb6bfcb45939178f2b0f
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-03-15 15:03:31 -0500

    udpated JS
    fixed/added conditional check for empty input fields

--- scriptHelper.js
+++ scriptHelper.js
@@ -7,10 +7,19 @@ onload = (event) =>{
     
     document.getElementById("formSubmit").addEventListener("click", function(event){
         
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+
+        
         event.preventDefault()
         console.log("Unicorns! Puppy Paws! Kitty Noses!")
-        if()
-        alert("All fields are required.")
+
+        if(pilotName.value === ""|| copilotName.value === ""|| fuelLevel.value === ""|| cargoMass.value === ""){
+            alert("All fields are required.")
+        }
+
     });
 }        
 
